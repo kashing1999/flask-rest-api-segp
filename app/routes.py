30eb1f72a428b151ead01ea87419cf657b93609d
Ka Shing <leekashing2107@gmail.com>
2020-02-20 14:36:03 +0800

    added a predict route, for now just always return cardboard

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app, jsonify, abort, request, db#, Image, transforms
 from app.models import Student, House
 from base64 import b64decode
+from hashlib import md5
+from datetime import datetime
 
 # TODO: Session cookie
 
@@ -45,6 +47,11 @@ def make_prediction():
     if request.json != None and 'recycable_image' in request.json:
         b64 = request.json['recycable_image']
         img = b64decode(b64)
+
+        filename = md5(img).hexdigest() + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M") + '.jpg'
+        filepath = "/tmp/data/" + filename
+
+        print(filename)
         with open('/tmp/test-out.jpg', 'wb') as f:
             f.write(img)
         return jsonify({'prediction': 'cardboard'}), 200
